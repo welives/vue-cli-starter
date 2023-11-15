3da59ac88c1cd43235a24b1e037101126487586b
Jandan <lutao1096@gmail.com>
2023-11-15 15:24:48 +0800

    请求模块

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,6 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
-    complexity: ['error', 10],
     'prettier/prettier': 'error',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
